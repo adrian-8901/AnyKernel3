8b6d51a458a51e33ecebd087682c4a8a3ec4c910
UsiFX <xprjkts@gmail.com>
2022-02-10 16:18:27 +0200

    AnyKernel : XKernel Changes
    Signed-off-by: UsiFX <xprjkts@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Predator Stormbreaker Kernel  by sohamsen  @ xda-developers
+kernel.string=XKernel by UsiFX @ github
 do.devicecheck=1
 do.modules=0
 do.systemless=1
